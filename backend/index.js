1245b11478a4a05432e16091709463e2e17c2e3b
sidj3012 <siddharth96d@gmail.com>
2025-01-22 23:03:25 +0530

    cors options update

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,11 @@ require('dotenv').config();
 //
 const app = express();
 
-const corsOptions = {
-  origin: [
-    'http://localhost:3000', 
-    'https://employee3.vercel.app/' // No trailing slash
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-};
+
 
 
 // Middleware
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 // Routes
